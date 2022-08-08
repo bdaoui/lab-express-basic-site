8ef910f52fc3883db474966a8cf731e43edb382a
Soufiane Bdaoui <soufianebdaoui@gmail.com>
2022-08-08 18:39:29 +0200

    Connected the main.css

--- app.js
+++ app.js
@@ -4,10 +4,22 @@ const app = express(); // here we are initializing express
 app.use(express.static("public")); // serve static files (css and images)
 
 app.get('/', (req, res) =>{
-    res.send("home page")
+    res.sendFile(__dirname + "/views/index.html")
+} );
+
+app.get('/About', (req, res) =>{
+    res.sendFile(__dirname + "/views/About.html")
+} );
+
+app.get('/Works', (req, res) =>{
+    res.sendFile(__dirname + "/views/Works.html")
+} );
+
+app.get('/PhotoGallery', (req, res) =>{
+    res.sendFile(__dirname + "/views/PhotoGallery.html")
 } );
 
 app.listen(3000, () => { 
-    console.log('listening on port 300!');
+    console.log('listening on port 300');
 });
 
